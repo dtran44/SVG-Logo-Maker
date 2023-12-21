4351a4b9307c713dc3065c93e462813fc70928ea
Diana Tran <diana.tran44@gmail.com>
2023-12-21 19:45:17 +1100

    updated JS

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # SVG-Logo-Maker
+
+## Description
+The goal of this project was to generate a quality README file by using a command-line application. Specifically, I learnt how to: 
+
+- Install and use node.js 
+- Use node.js to create prompts for the user 
+- Create a function to write the README file and to initialise the inquirer 
+
+
+## Installation
+
+N/A
+
+## Usage
+N/A
+
+## License
+
+N/A
+
+
+## References
+- Basic Shapes (2023) https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Basic_Shapes
+- SVG Tutorial (2023) https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial
+https://stackoverflow.com/questions/63151945/how-to-generate-an-svg-file-in-node-js-data-visualisation-using-rough-js
---
+++ index.js
@@ -0,0 +1,75 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const prompt = inquirer.createPromptModule();
+
+// Array of questions
+const questions = [
+  {
+    type: 'input',
+    name: 'text',
+    message: 'What 3 letter text would you like?',
+  },
+  {
+    type: 'input',
+    name: 'textColour',
+    message: 'What text colour would you like?',
+  },
+  {
+    type: 'list',
+    name: 'shape',
+    message: 'Choose a shape for your logo:',
+    choices: ['circle', 'triangle', 'square'],
+  },
+  {
+    type: 'input',
+    name: 'shapeColour',
+    message: 'What colour shape would you like?',
+  },
+];
+
+// Function to create SVG shapes based on user input
+function createShapes(answers) {
+  let shapeSVG = '';
+
+  if (answers.shape === 'circle') {
+    shapeSVG = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="150" cy="100" r="80" stroke-width="4" fill="${answers.shapeColour}" />
+      <text x="50%" y="50%" text-anchor="middle" dominant-baseline="central" fill="${answers.textColour}">${answers.text}</text>
+    </svg>`;
+  } else if (answers.shape === 'triangle') {
+    shapeSVG = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      <polygon points="150,20 50,180 250,180" stroke-width="2" fill="${answers.shapeColour}" />
+      <text x="150" y="120" text-anchor="middle" dominant-baseline="central" fill="${answers.textColour}">${answers.text}</text>
+    </svg>`;
+  } else if (answers.shape === 'square') {
+    shapeSVG = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      <rect x="50" y="50" width="200" height="200" stroke-width="4" fill="${answers.shapeColour}" />
+      <text x="150" y="150" text-anchor="middle" dominant-baseline="central" fill="${answers.textColour}">${answers.text}</text>
+    </svg>`;
+  }
+
+  return shapeSVG;
+}
+
+// Function to write SVG shapes to a file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(`${fileName} has been created successfully.`);
+  });
+}
+
+// Function to initialise the application
+function init() {
+  prompt(questions).then((answers) => {
+    const shapeSVG = createShapes(answers);
+    writeToFile('logo.svg', shapeSVG); 
+  });
+}
+
+// Initialise the application
+init();
